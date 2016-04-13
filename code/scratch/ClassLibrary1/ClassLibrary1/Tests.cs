021cfd04a8f3f6665d80395ba89f738910a803e3
draptik <socialcoding@pdrechsler.de>
2016-04-13 07:52:56 +0200

    cleanup

--- code/scratch/ClassLibrary1/ClassLibrary1/Tests.cs
+++ code/scratch/ClassLibrary1/ClassLibrary1/Tests.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using ClassLibrary1.Domain;
 using ClassLibrary1.Events;
 using ClassLibrary1.Framework;
 using FluentAssertions;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using Xunit;
 
 namespace ClassLibrary1
 {
     public class Tests
     {
-
         [Fact]
         public void SimpleTest()
         {
@@ -25,12 +22,21 @@ namespace ClassLibrary1
             var dbObjects = new List<DbObject>
             {
                 new DbObject {Type = typeof (CustomerCreated).AssemblyQualifiedName, Payload = jsonCreate},
-                new DbObject {Type = typeof (CustomerAddressChanged).AssemblyQualifiedName, Payload = jsonAddressChanged},
-                new DbObject {Type = typeof (CustomerAddressChanged).AssemblyQualifiedName, Payload = jsonAddressChangedAgain}
+                new DbObject
+                {
+                    Type = typeof (CustomerAddressChanged).AssemblyQualifiedName,
+                    Payload = jsonAddressChanged
+                },
+                new DbObject
+                {
+                    Type = typeof (CustomerAddressChanged).AssemblyQualifiedName,
+                    Payload = jsonAddressChangedAgain
+                }
             };
 
             var domainEvents = new List<DomainEvent>();
-            foreach (var dbObject in dbObjects) {
+            foreach (var dbObject in dbObjects)
+            {
                 var payload = JsonConvert.DeserializeObject(dbObject.Payload, Type.GetType(dbObject.Type));
                 domainEvents.Add(payload as DomainEvent);
             }
@@ -50,23 +56,5 @@ namespace ClassLibrary1
             public string Type { get; set; }
             public string Payload { get; set; }
         }
-
-        // Just an idea
-        //public static class MyConverter
-        //{
-            
-
-        //    public static T Convert<T>(dynamic source) where T : class, new() // or DomainEvent
-        //    {
-        //        T t = new T();
-        //        Update(source, t);
-        //        return t;
-        //    }
-
-        //    private static void Update<T>(dynamic source, T o) where T : class
-        //    {
-
-        //    }
-        //}
     }
 }
